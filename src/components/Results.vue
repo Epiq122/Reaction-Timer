e36d90a5f2ff11e0521dc1a573881087140c0ce4
Rob Gleason <51282111+Epiq122@users.noreply.github.com>
2021-01-17 22:13:10 -0700

    Finished Game

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Blazing Fast';
+    } else if (this.score < 400) {
+      this.rank = 'Decently Fast';
+    } else {
+      this.rank = 'Are you fit for duty?';
+    }
+  },
+};
+</script>
+
+<style scoped>
+.rank {
+  font-size: 1.4rem;
+  color: crimson;
+  font-weight: bold;
+}
+</style>
